974e177331b5db3e9d50dd6ad83621c8379c92d6
PJ Dietz <pjdietz@gmail.com>
2015-07-11 17:37:07 -0400

    Parse method, path, and body from request

--- rester/parse.py
+++ rester/parse.py
@@ -15,6 +15,46 @@ class RequestParser():
             head = request
             body = None
 
-        return self.request_factory(body=body)
+        headers = head.split("\n")
+        request_line = headers[0]
+        headers = headers[1:]
+        headers = self.parse_headers(headers)
 
+        # The first line of the head is the request line.
+        method, path, protocol = self.parse_request_line(request_line)
 
+        return self.request_factory(
+            method=method,
+            path=path,
+            protocol=protocol,
+            headers=headers,
+            body=body)
+
+    @staticmethod
+    def parse_request_line(line):
+        """Return a tuple of method, path, protocol"""
+
+        spaces = line.count(" ")
+        # Entire line is the path
+        if spaces == 0:
+            return "GET", line, "HTTP/1.1"
+        # Assume Method Path Protocol
+        elif spaces == 2:
+            return line.split(" ")
+        # Treat as Method Path. Any other text is discarded.
+        else:
+            method, path = line.split(" ")
+            return method, path, "HTTP/1.1"
+
+    @staticmethod
+    def parse_headers(lines):
+        """Return a dict of headers given header lines"""
+        headers = {}
+        for line in lines:
+            name, value = line.split(":")
+            name = name.strip()
+            value = value.strip()
+            if not name in headers:
+                headers[name] = []
+            headers[name].append(value)
+        return headers
--- test/test_parse.py
+++ test/test_parse.py
@@ -14,14 +14,60 @@ Host: my.host.com
 POST_PLAIN_TEXT = """
 POST /path HTTP/1.1
 Content-type: text/plain
+X-custom: Custom header
+X-custom: Another header
 
 This is the body
+"""
+
+NO_PROTOCOL = """
+GET /my.path.with.no.protocol
+"""
+
+PATH_ONLY = """
+/path-only
 """
 
 # -----------------------------------------------------------------------------
 
 
 class RequestParserTest(unittest.TestCase):
+
+    ###
+    # Method
+    ###
+
+    def test_parses_method(self):
+
+        factory = mock.Mock()
+        parser = parse.RequestParser(factory)
+
+        requests = [
+            ("GET", GET),
+            ("POST", POST_PLAIN_TEXT),
+            ("GET", NO_PROTOCOL),
+            ("GET", PATH_ONLY)
+        ]
+
+        for method, request in requests:
+            parser.parse(request)
+            args, kwargs = factory.call_args
+            self.assertEqual(kwargs["method"], method)
+
+    ###
+    # Headers
+    ###
+
+    def test_parses_headers(self):
+
+        factory = mock.Mock()
+        parser = parse.RequestParser(factory)
+        parser.parse(POST_PLAIN_TEXT)
+        args, kwargs = factory.call_args
+        self.assertEqual(kwargs["headers"], {
+            "Content-type": ["text/plain"],
+            "X-custom": ["Custom header", "Another header"]
+        })
 
     ###
     # Body
@@ -31,13 +77,15 @@ class RequestParserTest(unittest.TestCase):
         factory = mock.Mock()
         parser = parse.RequestParser(factory)
         parser.parse(POST_PLAIN_TEXT)
-        factory.assert_called_with(body="This is the body")
+        args, kwargs = factory.call_args
+        self.assertEqual(kwargs["body"], "This is the body")
 
     def test_parses_no_body(self):
         factory = mock.Mock()
         parser = parse.RequestParser(factory)
         parser.parse(GET)
-        factory.assert_called_with(body=None)
+        args, kwargs = factory.call_args
+        self.assertEqual(kwargs["body"], None)
 
 if __name__ == '__main__':
     unittest.main()
